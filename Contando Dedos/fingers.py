dc6b76f9d1476c7b55a7da6e5ac69d645ae7e78c
danrlei-oliveira <danrlei.jesus@hotmail.com>
2023-09-08 16:54:23 -0300

    first part of finger counter

---
+++ Contando Dedos/fingers.py
@@ -0,0 +1,25 @@
+import cv2
+import mediapipe as mp
+try:
+    video = cv2.VideoCapture(0)
+
+    # mapping the hands
+    hand = mp.solutions.hands
+
+    # maximum number of hands for the algorithm to recognize, drawing the lines of the hands
+    Hand = hand.Hands(max_num_hands=1)
+    mp_draw = mp.solutions.drawing_utils
+
+    while True:
+        check, img = video.read()
+        if not check or img is None:
+            continue
+
+        # converting the image from BGR to RGB for processing in MediaPipe
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = Hand.process(imgRGB)
+
+        cv2.imshow('Imagem', img)
+        cv2.waitKey(1)
+except Exception as e:
+    print(f'Error: {e}')
